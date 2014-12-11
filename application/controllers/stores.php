56eb3941c2bdeb226e0e9c687996c4f25eea98e8
BenKai3 <benjaminerkan@gmail.com>
2014-12-10 22:31:24 -0800

    beating you

--- application/controllers/stores.php
+++ application/controllers/stores.php
@@ -12,44 +12,30 @@ class Stores extends CI_Controller {
 	{
 		$this->load->model('Store');
 		$display['products'] = $this->Store->get_all_products();
-		$display['categories'] = $this->Store->get_all_categories();
+		$categories = $this->Store->get_all_categories();
+		$this->session->set_userdata('categories', $categories);
 		$this->load->view('store', $display);
 	}
 	public function category_store($id) {
 		$this->load->model('Store');
 		$display['category'] = $id;
+		if ($this->input->post('selected_order')) {
+		$display['products'] = $this->Store->get_all_in_category_by_order($id);
+		}
+		else
+		{
 		$display['products'] = $this->Store->get_all_in_category($id);
+		}
 		$this->load->view('store', $display);
 	}
 	public function product_store($id) {
-		// // pagination, with explanation @ https://ellislab.com/codeigniter/user-guide/libraries/pagination.html
-		// //  more pagination tips https://github.com/soyosolution/CodeIgniter-pagination-library
-		// // more more pagination tips http://www.storycon.us/ci3/libraries/pagination.html
-		$this->load->library('pagination');
-		$config['base_url'] = 'http://localhost:8888/stores/category_store';
-		$config['total_rows'] = 50;
-		$config['per_page'] = 10; 
-		$config['num_links'] = 2;
-		$config['first_link'] = 'first';
-		$config['last_link'] = 'last';
-		$config['next_link'] = 'next';
-		$config['prev_link'] = '&lt;';
-		$config['display_pages'] = FALSE;
-		// $config['anchor_class'] = 'pagination_links';
-		$this->pagination->initialize($config); 
-		// echo $this->pagination->create_links();
-		//end of pagination code
-
 		$this->load->model('Store');
-		$this->Store->get_all_in_category();
-		$display['category'] = $id;
 		$display['products'] = $this->Store->product_buy($id);
-		$this->load->view('store', $display);
+		$this->load->view('product', $display);
 	}
 	public function product_buy($id) {
 		$this->load->model('Store');
 		$display['id'] = $this->Store->product_buy($id);
-		// $display['category'] = $id;
 		$this->load->view('cart', $display);
 	}
 	public function show_cart() {
@@ -86,8 +72,29 @@ class Stores extends CI_Controller {
 	}
 	public function search_product() {
 		$name = $this->input->post('name');
+
 		$this->load->model('Store');
-		$display['products'] = $this->Store->get_product_by_name($name);
-		$this->load->view('product', $display);
+		$display['products'] = $this->Store->get_product_by_name($name, selectiontype);
+		$display['name'] = $this->input->post('name');
+		$this->load->view('store', $display);
+	}
+	public function order_by() {
+		$this->load->model('Store');
+		$word_search = $this->input->post('word_search');
+		$category = $this->input->post('category');
+		$selected_order = $this->input->post('selected_order');
+		if (!isset($word_search)) {
+			$word_search = NULL;
+		}
+		if (!isset($category)) {
+			$category = NULL;
+		}
+		if (!isset($selected_order)) {
+			$selected_order = NULL;
+		}
+		$display['products'] = $this->Store->get_category_with_search_by_order($selected_order, $word_search, $category);
+		$categories = $this->Store->get_all_categories();
+		$this->session->set_userdata('categories', $categories);
+		$this->load->view('store', $display);
 	}
 }//end of Controller curly
--- application/views/product.php
+++ application/views/product.php
@@ -1,7 +1,7 @@
 	<a href="/default_controller">Go Back</a>
-	<?php foreach ($products as $product) { ?>
-		<h1><?=$product['name'];?></h1>
-		<?php } ?>
+ 			<h1>
+				<?=$products['name'];?>
+			</h1>
 	<div class="left_div">
 		<!-- <img src="#" alt="main_photo">
 
@@ -12,28 +12,26 @@
 		<img src="#" alt="side_photo"> -->
 	</div>
 	<div class="right_div">
-		<?php foreach ($products as $product) { ?>
-			$<?=$product['price'];?><br>
-			<?=$product['description'];?><br>
-		<?php } ?>
-		<form>
-			<select>
-				<option>$products['quantity'];</option>
-				<option>$products['quantity'];</option>
-				<option>$products['quantity'];</option>
+			$<?=$products['price'];?><br>
+			<?=$products['description'];?><br>
+		<form action="/add_cart/<?= $products['id']?>" method="post">
+			<select name="qty">
+				<?php for ($i=0; $i <= $products['inventory_count']; $i++) { ?>
+					<option value="<?= $i ?>"><?= $i; ?></option>
+				<?php } ?>
 			</select>
 			<input type="submit" name="BUYYYYYYYYY">
 		</form>
 	</div>
-
 	<h2>Similar Items</h2>
 	<div class="bottom_div">
 		<!-- <div class="each_product">
-<?php 		for($i=0; $i<6; $i++) { ?>
+<?php 	for($i=0; $i<6; $i++)
+		{ ?>
 			<?=$products['name'];?><br>
-			<?=$products['price'];?><br>
+			$<?=$products['price'];?><br>
 			<?=$products['description'];?><br>
-<?php 		} ?>
+<?php 	} ?>
 		</div> -->
 	</div>
 </div><!-- close content div -->
